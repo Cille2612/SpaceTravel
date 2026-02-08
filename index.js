3fd00121fb124c55e3cc426840b4369ddb79d25c
Cille2612 <caecilia-werle@gmail.com>
2026-02-08 18:08:54 +0100

    Fix: Port für Deployment angepasst und .gitignore hinzugefügt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const API_URL = "https://api.spacexdata.com/v3/history";
 
